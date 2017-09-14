cb15726800fa3dcceb379babe2f6c5d85259e681
BenK <ebaykazemi@googlemail.com>
2017-09-14 19:01:46 +0100

    Added note on changing the PWM Frequency
    Have added change to pwm freq for teensy

--- examples/passthrough/passthrough.ino
+++ examples/passthrough/passthrough.ino
@@ -4,13 +4,17 @@
 #define PWM_PIN 5
 
 Adafruit_DRV2605 drv;
-int pwmValue = 0; 
+int pwmValue = 254; 
 int increments = 1;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("PWM Passthrough Test");
   drv.pwmPassthrough();
+
+  //if you're on teensy, 
+  //it's a good idea to use this function that adjusts the pwm frequency
+  //analogWriteFrequency(PWM_PIN, 15000);// PWM Freq for motor   
 }
 
 void loop() {
@@ -22,9 +26,8 @@ void loop() {
 
   pwmValue = pwmValue + increments;
 
-  if (pwmValue <= 0 || pwmValue >= 255) {
+  if (pwmValue <= 0 || pwmValue >= 255) 
     increments = -increments;
-  }
   
   delay(30);
 }
