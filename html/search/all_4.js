e22cf9dcefc7a32f4ad753373e5a9d9429bddfe1
Doxygen CI <ci-arduino@invalid>
2022-10-04 18:15:56 +0000

    Deploy docs to GitHub Pages from commit dbec29c748
    Commit: dbec29c7480b28f54ac8651c1464e4469c7a0eb1
    GitHub Actions run: 3184425738

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['init',['init',['../class_adafruit___d_r_v2605.html#a3de94ee90b624dca88642828354d427c',1,'Adafruit_DRV2605']]]
+  ['init',['init',['../class_adafruit___d_r_v2605.html#adf4aaba9319a6519ceda515593b99085',1,'Adafruit_DRV2605']]]
 ];
--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___d_r_v2605.html#a7b1609e6bbe37b521dc3cd0f9f21aa3d',1,'Adafruit_DRV2605']]]
+  ['begin',['begin',['../class_adafruit___d_r_v2605.html#a26c2211e6d16ac90107cc397a79574fb',1,'Adafruit_DRV2605']]]
 ];
